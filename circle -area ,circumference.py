7aee7b9b7142cee31ba7af23aecb9baf2bb7afa4
amitpr58 <111772936+amitpr58@users.noreply.github.com>
2022-08-22 22:18:04 +0530

    Create circle -area ,circumference.py

---
+++ circle -area ,circumference.py
@@ -0,0 +1,7 @@
+#calculate the area of circle & circumference
+r=float(input("enter the radius of the circle\n="))
+pi=3.14
+area=pi*r*r
+circumference=2*pi*r
+print("the area of circle",pi*r*r)
+print("the circumference of the circle",2*pi*r)
