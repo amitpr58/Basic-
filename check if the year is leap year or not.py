6a4d224efbc4a1e0bc1325b9be30ffc78dcbd7b0
amitpr58 <111772936+amitpr58@users.noreply.github.com>
2022-08-22 23:45:08 +0530

    Create check if the year is leap year or not.py

---
+++ check if the year is leap year or not.py
@@ -0,0 +1,10 @@
+#check if the year is leap year or not
+x=float(input("enter the any year\n="))
+if x%4==0 :
+    print("%d is the leap year "%x)
+  
+#elif x<0:
+    #print("%d is the number is negative number "%x)
+    
+else :
+    print("%d is not a leap year"%x)
