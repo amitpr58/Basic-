2ac88f2ea31c91f2bd867fb093f6d289753edbfc
amitpr58 <111772936+amitpr58@users.noreply.github.com>
2022-08-22 23:06:48 +0530

    Create given number is divisible by 5 and 2 or not.py

---
+++ given number is divisible by 5 and 2 or not.py
@@ -0,0 +1,10 @@
+#given number is divisible by 5 and 2 or not
+x=int(input("enter any number\n="))
+if x%5==0&x%2==0 :
+    print("%d is divisible by 5"%x)
+   
+elif x%2==0:
+    print("%d is divisible by 2"%x)
+  
+else:
+       print("%d is not divisible by 5 and 2"%x)
