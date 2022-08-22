7b33b715d366b675a49c3f2066b24eb8625d1581
amitpr58 <111772936+amitpr58@users.noreply.github.com>
2022-08-22 23:38:03 +0530

    Create check whether a given number is positive negative or zero.py

---
+++ check whether a given number is positive negative or zero.py
@@ -0,0 +1,10 @@
+#check whether a given number is positive negative or zero
+x=float(input("enter any number\n="))
+if x>0 :
+    print("%d is the number is positivr number "%x)
+  
+elif x<0:
+    print("%d is the number is negative number "%x)
+    
+else :
+    print("%d is the number is zero"%x)
