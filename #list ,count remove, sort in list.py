6394934cbc00cb4dc7291c36a823cd4504e02233
amitpr58 <111772936+amitpr58@users.noreply.github.com>
2022-11-21 19:47:48 +0530

    Create #list ,count remove, sort in list.py

---
+++ #list ,count remove, sort in list.py
@@ -0,0 +1,6 @@
+#list ,count remove, sort in list
+l=[5,8,68,5,7,5,8,8,5,2,4]
+l.count(2)
+print("in this sequence outcome in totol:",l.count(2))
+l.index(2)
+print("in this sequence outcome in position:",l.index(2))
