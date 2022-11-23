829c8a587b4c02940069925612a4f604b5924d84
amitpr58 <111772936+amitpr58@users.noreply.github.com>
2022-11-23 20:54:49 +0530

    Create #in list total number in sequence.py

---
+++ #in list total number in sequence.py
@@ -0,0 +1,9 @@
+#in list total number in sequence
+#list ,count remove, sort in list
+l=[5,8,68,5,7,5,8,8,5,2,4]
+l.count(2)
+print("in this sequence outcome in totol:",l.count(2))
+l.index(4)
+print("in this sequence outcome in position:",l.index(4))
+len(l)
+print("in this total number in sequence:",len(l))
