fceb90e231817682ed7e4c12994ed375ee4948ae
amitpr58 <111772936+amitpr58@users.noreply.github.com>
2023-01-18 00:42:12 +0530

    Create #STRING IN  SLICING OPERATOR.py

---
+++ #STRING IN  SLICING OPERATOR.py
@@ -0,0 +1,11 @@
+#STRING IN  SLICING OPERATOR
+
+a1= "amit prajapati az"
+print(len(a1))
+print(a1.index('i'))
+print(a1.count('a'))
+print(a1[5:15])
+print(a1[1:-2])
+print(a1[-3:18])
+print(a1[17::-1])
+print(a1[::-1])
