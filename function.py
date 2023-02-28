98c0a2aea98f785a9371827af69c46567b97e160
amitpr58 <111772936+amitpr58@users.noreply.github.com>
2023-02-28 21:46:27 +0530

    Update function.py

--- function.py
+++ function.py
@@ -3,7 +3,9 @@ def add():
     b=eval(input("Enter Second number"))
     c=a+b
     print("sum is",c)
-    
+print("Hello this is first add code")    
 add()
+print("this is a line after first code of add")
 add()
+print("this is last and final code of add")
 add()
