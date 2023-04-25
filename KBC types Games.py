7e80d9d5e471a45695b4cbec6e475014e53d4e81
amitpr58 <111772936+amitpr58@users.noreply.github.com>
2023-04-25 21:08:08 +0530

    Create KBC types Games.py

---
+++ KBC types Games.py
@@ -0,0 +1,44 @@
+# KBC types Games
+#Question and Answer are pre define 
+
+
+print("Question = 1")
+a=["Which Country you belong ?"]
+
+
+print(a)
+
+c=(input("Enter your answer:"))
+if c==("INDIA"):
+  print("Your answer is right")
+else:
+  print("Your answer is wrong")
+
+
+  print("question = 2")
+
+a=["What is the study of mushrooms called?"]
+b=["Mythology","Mycology","Mushroomology"]
+c=a+b
+print(c)
+
+c=(input("Enter your answer:"))
+if c==("Mycology"):
+  print("Your answer is right")
+else:
+  print("Your answer is wrong")
+
+print("question = 3")
+
+a=["What is the name of the tallest grass on earth?"]
+b=["Bamboo","Peepal tree","Mango tree"]
+c=a+b
+print(c)
+
+c=(input("Enter your answer:"))
+if c==("Bamboo"):
+  print("Your answer is right")
+else:
+  print("Your answer is wrong")
+
+print("Thanks for playing this simple quiz and congratulations for winning 50k")
